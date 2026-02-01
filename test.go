9a43473f2d2b953ab60d358f08b38a893812a938
Torsten <torsten@rubydesign.fi>
2026-02-02 02:55:33 +0800

    going nowhere fast

--- test.go
+++ test.go
@@ -2,29 +2,36 @@ package main
 
 import (
 	"github.com/antlr4-go/antlr/v4"
-	"./parser"  // Note that with modules you may not be able to use a relative immport path
+  "kigo-som/parser"
 	"os"
-	"fmt"
 )
 
-type TreeShapeListener struct {
-	*parser.BaseJSONListener
+type Visitor struct {
+  *parser.BaseSomVisitor
 }
 
-func NewTreeShapeListener() *TreeShapeListener {
-	return new(TreeShapeListener)
+type SomVisitor struct {
+    parser.SomVisitor
 }
 
-func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+func (v *SomVisitor) VisitClassdef(ctx *parser.ClassdefContext) interface{} {
+
+    return ctx
 }
 
 func main() {
 	input, _ := antlr.NewFileStream(os.Args[1])
-	lexer := parser.NewJSONLexer(input)
+  lexer := parser.NewSomLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer,0)
-	p := parser.NewJSONParser(stream)
+  p := parser.NewSomParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	tree := p.Json()
-	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+
+
+  tree := p.Classdef()
+
+  // To use:
+  visitor := &SomVisitor{}
+  result := visitor.Visit(tree).(int)
+  result = result - 1
+  return
 }
