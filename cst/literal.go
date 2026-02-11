1c3ef4f439b929c5a2c4be3811b0c13b7dd3142a
Torsten <torsten@rubydesign.fi>
2026-02-11 17:23:04 +0700

    completes literal numbers

--- cst/literal.go
+++ cst/literal.go
@@ -50,25 +50,34 @@ func MakeLiteralArray(ctx *parser.LiteralArrayContext) ([]*Literal){
 func MakeLiteralSymbol(ctx *parser.LiteralSymbolContext) (string){
   return ""
 }
+
 func MakeLiteralString(ctx *parser.LiteralStringContext) (string){
   return ""
 }
-func MakeLiteralNumber(ctx *parser.LiteralNumberContext) (int , int , float64){
-  if negative_ctx := ctx.NegativeDecimal() ; negative_ctx != nil {
-    //negative := negative_ctx.(*parser.NegativeDecimalContext)
 
-  } // else
-  decimal_ctx := ctx.LiteralDecimal().(*parser.LiteralDecimalContext)
+func MakeLiteralDecimal(decimal_ctx *parser.LiteralDecimalContext) (int , int , float64){
   if literal_ctx := decimal_ctx.LiteralInteger() ; literal_ctx != nil {
     int_str := literal_ctx.(*parser.LiteralIntegerContext).Integer().GetText()
     int, _ := strconv.Atoi(int_str)
+    log.Println("lit int " , int , reflect.TypeOf(int))
     return 4, int , 0.0
   } else {
     double_ctx := decimal_ctx.LiteralDouble().(*parser.LiteralDoubleContext)
     double_str := double_ctx.Double().GetText()
+    log.Println("lit float " , double_str , reflect.TypeOf(double_str))
     double, _ := strconv.ParseFloat(double_str, 64)
     return 5, 0 , double
   }
+}
+
+func MakeLiteralNumber(ctx *parser.LiteralNumberContext) (int , int , float64){
+  if negative_ctx := ctx.NegativeDecimal() ; negative_ctx != nil {
+    decimal_ctx := negative_ctx.LiteralDecimal().(*parser.LiteralDecimalContext)
+    typ , int , float := MakeLiteralDecimal(decimal_ctx)
+    return typ , -int , -float
+  } // else
+  decimal_ctx := ctx.LiteralDecimal().(*parser.LiteralDecimalContext)
+  return MakeLiteralDecimal( decimal_ctx)
 }
 
 func MakeLiteral(ctx *parser.LiteralContext) (*Literal){
@@ -82,14 +91,12 @@ func MakeLiteral(ctx *parser.LiteralContext) (*Literal){
     log.Println("symbol " , symbol , reflect.TypeOf(symbol))
     return &Literal{2 , nil ,symbol  , 0, 0}
   }
-  if string_ctx := ctx.LiteralSymbol() ; string_ctx != nil {
-    string := MakeLiteralSymbol( string_ctx.(*parser.LiteralSymbolContext) )
+  if string_ctx := ctx.LiteralString() ; string_ctx != nil {
+    string := MakeLiteralString( string_ctx.(*parser.LiteralStringContext) )
     log.Println("string " , string , reflect.TypeOf(string))
     return &Literal{2 , nil ,string  , 0, 0}
   }
   number_ctx := ctx.LiteralNumber()
   typ , int , float := MakeLiteralNumber( number_ctx.(*parser.LiteralNumberContext) )
-  log.Println("int " , int , reflect.TypeOf(int))
-  log.Println("float " , float , reflect.TypeOf(float))
   return &Literal{typ , nil , "" , int, float}
 }
