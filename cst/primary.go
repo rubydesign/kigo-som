d55f5e3fde0b2257bd56fc3c99b82654d823a7f9
Torsten <torsten@rubydesign.fi>
2026-02-12 11:44:27 +0700

    Nested term done and some collapsing
    NestTerm is really just and Expression, collaped
    Variable is really just a string, collapsed
    BlockPattern resolves to a list of strings, collapsed

--- cst/primary.go
+++ cst/primary.go
@@ -8,45 +8,36 @@ import (
 
 // primary:
 //     variable | nestedTerm | nestedBlock | literal;
+// nestedTerm:
+//     NewTerm expression EndTerm;
 
+// collaping nestedTerm into Expression
 
-type Variable struct {
-  variable string
-}
-type NestedTerm struct {
-
-}
-type BlockPattern struct {
-
-}
 type Primary struct {
-  variable    *Variable
-  nestedTerm  *NestedTerm
+  variable    string
+  nestedTerm  *Expression
   nestedBlock *NestedBlock
   literal     *Literal
-}
-
-func MakeNestedTerm(ctx *parser.NestedTermContext) (*NestedTerm){
-  return &NestedTerm{}
 }
 
 func MakePrimary(ctx *parser.PrimaryContext) (*Primary){
   if variable_ctx := ctx.Variable() ; variable_ctx != nil {
     name := variable_ctx.(*parser.VariableContext).Identifier().GetText()
-    return &Primary{&Variable{name} , nil , nil , nil}
+    return &Primary{name , nil , nil , nil}
   }
   if nestedTerm_ctx := ctx.NestedTerm() ; nestedTerm_ctx != nil {
-    nestedTerm := MakeNestedTerm( nestedTerm_ctx.(*parser.NestedTermContext) )
-    log.Println("nestedTerm " , nestedTerm , reflect.TypeOf(nestedTerm))
-    return &Primary{nil , nestedTerm , nil , nil}
+    nested_expression := nestedTerm_ctx.Expression()
+    expression := MakeExpression(nested_expression.(*parser.ExpressionContext))
+    log.Println("nested_expression " , nested_expression , reflect.TypeOf(nested_expression))
+    return &Primary{"" , expression , nil , nil}
   }
   if nestedBlock_ctx := ctx.NestedBlock() ; nestedBlock_ctx != nil {
     nestedBlock := MakeNestedBlock( nestedBlock_ctx.(*parser.NestedBlockContext) )
     log.Println("nestedBlock " , nestedBlock , reflect.TypeOf(nestedBlock))
-    return &Primary{nil , nil , nestedBlock , nil}
+    return &Primary{"" , nil , nestedBlock , nil}
   }
   literal_ctx := ctx.Literal() // by now this can't be anything else
   literal := MakeLiteral( literal_ctx.(*parser.LiteralContext) )
-  return &Primary{ nil , nil , nil , literal}
+  return &Primary{ "" , nil , nil , literal}
 
 }
