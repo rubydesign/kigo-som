beb657db41aa39c86150affa77c57032dda80e48
Torsten <torsten@rubydesign.fi>
2026-02-12 18:34:25 +0700

    done unary and start binary, have to rethink structs

--- cst/messages.go
+++ cst/messages.go
@@ -2,8 +2,8 @@ package cst
 
 import (
     "kigo-som/parser"
-    // "log"
-    // "reflect"
+    "log"
+    "reflect"
 )
 
 // messages:
@@ -26,12 +26,35 @@ import (
 //    binaryOperand binaryMessage*;
 
 type Message  struct {
+  typ int // 1 unary , 2 binary , 3 keyword
+  message []string  // selectors and args (alternate)
+}
 
+func MakeBinaryMessage(ctx *parser.BinaryMessageContext) (*Message){
+  selector_ctx := ctx.BinarySelector() //IBinarySelectorContext {
+  selector := MakeBinarySelector(selector_ctx.(*parser.BinarySelectorContext))
+  message := &Message{2 , selector}
+  operarand_ctx := ctx.BinaryOperand() //IBinaryOperandContext {
+  primary_ctx := operarand_ctx.Primary()
+  //primary := 
+  MakePrimary(primary_ctx.(*parser.PrimaryContext))
+  return message
 }
 
 func MakeMessages(ctx parser.IMessagesContext) ([]*Message){
   messages := make([]*Message , 0 ,  3)
   if ctx == nil { return messages }
-  //message_ctx := ctx.(*parser.MessagesContext)
+  for i := range ctx.AllUnaryMessage() {
+    unary_ctx := ctx.UnaryMessage(i)
+    method_name := unary_ctx.UnarySelector().Identifier().GetText()
+    log.Println("method_name" , method_name , reflect.TypeOf(method_name))
+    messages = append( messages , &Message{1 , []string{method_name} } )
+  }
+  for bi := range ctx.AllBinaryMessage() {
+    binary_ctx := ctx.BinaryMessage(bi)
+    message := MakeBinaryMessage( binary_ctx.(*parser.BinaryMessageContext) )
+    log.Println("message" , message , reflect.TypeOf(message))
+    messages = append( messages , message )
+  }
   return messages
 }
