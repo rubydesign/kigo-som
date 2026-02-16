34ad2ab6b2b1427e41bb491f2fd7fac534f9a255
Torsten <torsten@rubydesign.fi>
2026-02-16 12:00:17 +0700

    fold message types , not using interface
    interfaces were panicing and didn't know why

--- cst/messages.go
+++ cst/messages.go
@@ -25,68 +25,68 @@ import (
 // formula:
 //    binaryOperand binaryMessage*;
 
-type Message  interface {
-}
-
-type UnaryMessage struct{
+type UnaryMessage struct {
   message []string  // selectors and args (alternate)
+}
+type BinaryMessage struct {
+  selector string
+  operand *BinaryOperand
+}
+type KeywordMessage struct {
+  keywords []string
+  formuli  []*Formula
+}
+type Message struct {
+  typ int  // 1 unary , 2 binary , 3 keyword
+  message []string
+  operand *BinaryOperand  // only for binary
+  formuli  []*Formula // only for keyword
 }
 type BinaryOperand struct {
   primary *Primary
   unary   []string
 }
-type BinaryMessage struct{
-  selector string
-  operand *BinaryOperand
-}
 type Formula struct{
   operand *BinaryOperand
-  messages []*BinaryMessage;
+  messages []*Message; // binary
 }
 
-type KeywordMessage struct {
-  keywords []string
-  formuli  []*Formula
-}
-
-func PrintFormula(pre string , formula *Formula){
-  PrintBinaryOperand( pre , formula.operand)
+func PrintFormula(pre string , typ string , formula *Formula){
+  PrintBinaryOperand( pre , typ + " BO" , formula.operand)
   for i := range formula.messages {
     message := formula.messages[i]
-    fmt.Println(pre + " binary: ", message.selector   )
-    PrintBinaryOperand( pre , message.operand)
+    fmt.Println(pre + typ + " binary: ", message.message   )
+    PrintBinaryOperand( pre , "BO", message.operand)
   }
 }
 
-func PrintBinaryOperand( pre string, operand *BinaryOperand){
-  fmt.Println(pre + " primary: " )
-  PrintPrimary( "  " + pre , operand.primary)
-  fmt.Println(pre + " unaries: ", strings.Join(operand.unary , ".") )
+func PrintBinaryOperand( pre string, typ string , operand *BinaryOperand){
+  fmt.Println(pre + typ , " primary: " )
+  PrintPrimary( "  " + pre , typ + " PR" , operand.primary)
+  fmt.Println(pre + typ + " unaries: ", strings.Join(operand.unary , ".") )
 }
 
-func PrintMessage( pre string, mess Message){
-  switch mess.(type){
-  case UnaryMessage:
-    message := mess.(UnaryMessage)
+func PrintMessage( pre string, typ string, message *Message){
+  switch message.typ {
+  case 1:
     fmt.Println(pre + " unary: ", strings.Join(message.message , ".")   )
-  case BinaryMessage:
-    message := mess.(BinaryMessage)
-    fmt.Println(pre + " binary: ", message.selector   )
-    PrintBinaryOperand( pre , message.operand)
-  case KeywordMessage:
-    message := mess.(KeywordMessage)
-    fmt.Println(pre + " keywords: ", strings.Join(message.keywords , ".")   )
+  case 2:
+    fmt.Println(pre + " binary: ", message.message   )
+    PrintBinaryOperand( pre , "BO",message.operand)
+  case 3:
+    fmt.Println(pre + " keywords: ", strings.Join(message.message , ".")   )
     for i := range message.formuli {
       formula := message.formuli[i]
-      PrintFormula("  " + pre , formula)
+      PrintFormula("  " + pre ,"FO" , formula)
     }
+  default: panic("message type " )
   }//switch
 }
 
-func PrintMessages( pre string, messages []Message){
+func PrintMessages( pre string, typ string , messages []*Message){
   for i := range messages {
     message := messages[i]
-    PrintMessage("  " + pre , message)
+    PrintMessage("  " + pre , typ + " ME", message)
   }
 }
 
@@ -104,28 +104,29 @@ func MakeBinaryOperand(ctx *parser.BinaryOperandContext) (*BinaryOperand){
   return &BinaryOperand{primary , unary}
 }
 
-func MakeBinaryMessage(ctx *parser.BinaryMessageContext) (Message){
+func MakeBinaryMessage(ctx *parser.BinaryMessageContext) (*Message){
   selector_ctx := ctx.BinarySelector() //IBinarySelectorContext {
   selector := MakeBinarySelector(selector_ctx.(*parser.BinarySelectorContext))
   operarand_ctx := ctx.BinaryOperand() // {
   operand := MakeBinaryOperand(operarand_ctx.(*parser.BinaryOperandContext))
-  return &BinaryMessage{ selector , operand}
+  messages := []string{selector}
+  return &Message{ 2 , messages , operand , nil}
 }
 
 func MakeFormula(ctx *parser.FormulaContext) (*Formula)  {
   operarand_ctx := ctx.BinaryOperand()
   operand := MakeBinaryOperand(operarand_ctx.(*parser.BinaryOperandContext))
-  messages := make([]*BinaryMessage , 0 ,  3)
+  messages := make([]*Message , 0 ,  3)
   binaries_ctx:= ctx.AllBinaryMessage()
   for bi := range binaries_ctx {
     binary_ctx := binaries_ctx[bi]
     message := MakeBinaryMessage( binary_ctx.(*parser.BinaryMessageContext) )
     //fmt.Println("binary" , message , reflect.TypeOf(message))
-    messages = append( messages , message.(*BinaryMessage) )
+    messages = append( messages , message )
   }
   return &Formula{operand , messages}
 }
-func MakeKeywordMessage(ctx *parser.KeywordMessageContext) (Message){
+func MakeKeywordMessage(ctx *parser.KeywordMessageContext) (*Message){
   keywords := make([]string , 0,3)
   keywords_ctx := ctx.AllKeyword()
   for idx := range keywords_ctx {
@@ -140,18 +141,18 @@ func MakeKeywordMessage(ctx *parser.KeywordMessageContext) (Message){
     formula := MakeFormula(formula_ctx.(*parser.FormulaContext))
     formuli = append(formuli , formula )
   }
-  return &KeywordMessage{ keywords , formuli}
+  return &Message{ 3 , keywords , nil , formuli}
 }
 
-func MakeMessages(ctx parser.IMessagesContext) ([]Message){
-  messages := make([]Message , 0 ,  3)
+func MakeMessages(ctx parser.IMessagesContext) ([]*Message){
+  messages := make([]*Message , 0 ,  3)
   if ctx == nil { return messages }
   unaries_ctx := ctx.AllUnaryMessage()
   for i := range unaries_ctx {
     unary_ctx := unaries_ctx[i]
     method_name := unary_ctx.UnarySelector().Identifier().GetText()
     //fmt.Println("unary" , method_name , reflect.TypeOf(method_name))
-    messages = append( messages , &UnaryMessage{[]string{method_name} } )
+    messages = append( messages , &Message{ 1 , []string{method_name} , nil,nil } )
   }
   binaries_ctx := ctx.AllBinaryMessage()
   for bi := range binaries_ctx {
